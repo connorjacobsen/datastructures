e6cc55d32b616591e4b792da79548f42c34b519a
Connor Jacobsen <jacobsen.connor@gmail.com>
2014-06-17 03:37:38 -0700

    Update stack formatting

--- include/stack.hpp
+++ include/stack.hpp
@@ -17,16 +17,10 @@ public:
 	/** CLASS METHODS **/
 
 	// Public: Returns the integer size of the Stack.
-	unsigned int size()
-	{
-		return storage.size();
-	}
+	unsigned int size() { return storage.size(); }
 
 	// Public: Returns true if the stack is empty, false otherwise.
-	bool empty()
-	{
-		return storage.empty();
-	}
+	bool empty() { return storage.empty(); }
 
 	// Public: Return the value at the top of the Stack.
 	T top()
@@ -36,10 +30,7 @@ public:
 	}
 
 	// Public: Add an element to the Stack.
-	void push( T element )
-	{
-		storage.push_back( element );
-	}
+	void push( T element ) { storage.push_back( element ); }
 
 	// Public: Remove an element from the Stack and return its value.
 	T pop()
